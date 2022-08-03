14d808fb34c68f7591bbe313d79acfd818472fbf
Juliasamoylova <julia.g.samoylova@gmail.com>
2022-08-03 14:32:15 +0300

    Homework9

---
+++ 9.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число N:");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число M:");
+int second = Convert.ToInt32(Console.ReadLine());
+
+int SumNat(int first, int second)
+{
+    if (first == second) return first;
+    return (first + SumNat(first+1, second));
+}
+Console.WriteLine(SumNat(first, second));
---
+++ 9.2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+Console.Write("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+static int Acker(int m, int n)
+{
+    if (n < 0 || m < 0) throw new ArgumentOutOfRangeException();
+    if (n == 0) return m + 1;
+    if (m == 0) return Acker(n - 1, m);
+    return Acker(n - 1, Acker(n, m - 1));
+}
+Console.WriteLine(Acker(m, n));
